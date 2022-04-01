075743cdc06d9724334ca926b8a8ce4710d68897
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2022-04-01 16:02:03 +0200

    add loc parameter to shift x values

--- scripts/features.py
+++ scripts/features.py
@@ -7,6 +7,7 @@ with open("params.yaml", 'r') as params_file:
     params = yaml.safe_load(params_file)
 
 scale = params["scale"]
+loc = params["loc"]
 noise_precision = params["noise_precision"]
 prior_precision = params["prior_precision"]
 season_shift = params["season_shift"]
@@ -15,10 +16,10 @@ cycle_shift = params["cycle_shift"]
 
 def build_features(year):
     """Compute feature vector for a given year."""
-    rescaled = scale * year
+    rescaled = scale * (year - loc)
     return np.array([
-        [1.] * rescaled,
-        rescaled**4,
+        [1.] * len(rescaled),
+        rescaled**5,
         np.sin(2 * np.pi * (year + season_shift)),
         np.sin(2 * np.pi * (year + cycle_shift) / cycle_len),
         np.exp(rescaled),
