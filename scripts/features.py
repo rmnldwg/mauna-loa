29ff1b82c2c469032010256d96a031b4e13252a2
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2022-04-01 13:15:03 +0200

    put features in separate script

---
+++ scripts/features.py
@@ -0,0 +1,25 @@
+import numpy as np
+import yaml
+
+
+# load the (hyper-params)
+with open("params.yaml", 'r') as params_file:
+    params = yaml.safe_load(params_file)
+
+scale = params["scale"]
+noise_precision = params["noise_precision"]
+prior_precision = params["prior_precision"]
+season_shift = params["season_shift"]
+cycle_len = params["cycle_len"]
+cycle_shift = params["cycle_shift"]
+
+def build_features(year):
+    """Compute feature vector for a given year."""
+    rescaled = scale * year
+    return np.array([
+        [1.] * rescaled,
+        rescaled**4,
+        np.sin(2 * np.pi * (year + season_shift)),
+        np.sin(2 * np.pi * (year + cycle_shift) / cycle_len),
+        np.exp(rescaled),
+    ])
--- scripts/train.py
+++ scripts/train.py
@@ -5,23 +5,12 @@ test data (CO2 averages from the year `K` to today).
 """
 
 import argparse
-import yaml
 
 import pandas as pd
 import numpy as np
-import scipy as sp
 from scipy.linalg import cho_factor, cho_solve
 
-
-def build_features(year, cycle_freq, scale):
-    """Compute feature vector for a given year."""
-    rescaled = scale * year
-    return np.array([
-        [1.] * rescaled,
-        rescaled**4,
-        np.sin(rescaled * cycle_freq),
-        np.exp(rescaled),
-    ])
+from features import build_features, prior_precision, noise_precision
 
 
 if __name__ == "__main__":
@@ -30,29 +19,16 @@ if __name__ == "__main__":
                         help="Training data CSV file")
     parser.add_argument("-o", "--output", required=True,
                         help="Path to store the posterior weights & covariance")
-    parser.add_argument("-p", "--params", required=True,
-                        help="Path to parameter YAML file")
     args = parser.parse_args()
     
-    with open(args.params, 'r') as params_file:
-        params = yaml.safe_load(params_file)
-    
-    scale = params["scale"]
-    noise_precision = params["train"]["noise_precision"]
-    prior_precision = params["train"]["prior_precision"]
-    cycle_freq = params["train"]["cycle_freq"]
-    
+    # open the training file
     with open(args.input, 'r') as train_file:
         train_df = pd.read_csv(train_file, index_col=0)
     
     year = train_df["year"].values
     target = train_df["average"].values
     
-    features = build_features(
-        year=year,
-        scale=scale,
-        cycle_freq=cycle_freq,
-    )
+    features = build_features(year=year)
     num_features = features.shape[0]
     
     # prior on the weights
